1b1819cb8a7b7ab416eaf80b017685d6218ef230
SPIDERCLIENT <modingcypress@gmail.com>
2024-11-26 17:08:58 -0600

    AntiAfk.cs

--- Safety/AntiAfk.cs
+++ Safety/AntiAfk.cs
@@ -2,8 +2,3 @@ public static void AntiAFK()//UND
 {
     PhotonNetworkController.Instance.disableAFKKick = false;
 }
-
-public static void DisableAntiAFK()//UND
-{
-    PhotonNetworkController.Instance.disableAFKKick = true;
-}
