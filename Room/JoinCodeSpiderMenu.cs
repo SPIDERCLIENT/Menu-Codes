1a8406480dae3d270998c48f8aa55bb985bc8eac
SPIDERCLIENT <modingcypress@gmail.com>
2024-11-26 17:05:32 -0600

    JoinCodeSpiderMenu.cs

--- Room/JoinCodeSpiderMenu.cs
+++ Room/JoinCodeSpiderMenu.cs
@@ -1,1 +1,6 @@
+public static void JoinCode(string code)//UND
+{
+    PhotonNetworkController.Instance.AttemptToJoinSpecificRoom(code, JoinType.Solo);
+}
 
+new ButtonInfo { buttonText = "Join Code SPIDERMENU [UND]", method =() => Codes.JoinCode("SPIDERMENU"), isTogglable = false},
