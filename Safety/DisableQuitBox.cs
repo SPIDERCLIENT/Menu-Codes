df81bb9a38c8155ba3d0c3e7a6f804f8007a8534
SPIDERCLIENT <modingcypress@gmail.com>
2024-11-26 17:13:31 -0600

    Update DisableQuitBox.cs

--- Safety/DisableQuitBox.cs
+++ Safety/DisableQuitBox.cs
@@ -2,8 +2,3 @@ public static void DisableQuitBox()//UND
 {
     GameObject.Find("Environment Objects/TriggerZones_Prefab/ZoneTransitions_Prefab/QuitBox").SetActive(false);
 }
-
-public static void EnableQuitBox()//UND
-{
-    GameObject.Find("Environment Objects/TriggerZones_Prefab/ZoneTransitions_Prefab/QuitBox").SetActive(true);
-}
