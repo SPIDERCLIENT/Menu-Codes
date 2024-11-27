2da5c2dbfcc76ce6d1dd7f7b0527a98d0545b851
SPIDERCLIENT <modingcypress@gmail.com>
2024-11-26 22:51:20 -0600

    FixBeeArms.cs

---
+++ VrRig/FixBeeArms.cs
@@ -0,0 +1,5 @@
+public static void FixBeeArms()//UND
+{
+    GorillaLocomotion.Player.Instance.rightControllerTransform.transform.position += new Vector3(0, 0, 0);
+    GorillaLocomotion.Player.Instance.leftControllerTransform.transform.position += new Vector3(0, 0, 0);
+}
