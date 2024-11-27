397facfaefccd301c5404925e486ad2345d346e9
SPIDERCLIENT <modingcypress@gmail.com>
2024-11-26 22:52:42 -0600

    RightArmGun.cs

--- VrRig/RightArmGun.cs
+++ VrRig/RightArmGun.cs
@@ -171,3 +171,11 @@ public static void RightArmGun()//UND
         UnityEngine.Object.Destroy(pointer, Time.deltaTime);
     }
 }
+
+public static LineRenderer Line;
+
+public static int change17 = 1;
+
+public static int change8 = 1;
+
+public static GameObject pointer;
