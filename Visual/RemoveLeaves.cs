8473cffdf5687941015680ab7eb506fd5080f1f8
SPIDERCLIENT <modingcypress@gmail.com>
2024-11-26 22:10:09 -0600

    RemoveLeaves.cs

---
+++ Visual/RemoveLeaves.cs
@@ -0,0 +1,11 @@
+public static void RemoveLeaves()//UND
+{
+    foreach (GameObject l in Resources.FindObjectsOfTypeAll<GameObject>())
+    {
+        if (l.activeSelf && l.name.Contains("fallleaves"))
+        {
+            l.SetActive(false);
+            leaves.Add(l);
+        }
+    }
+}
