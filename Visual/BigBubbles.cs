b11ebed4c786f8b1fd4e85e0af0d85acc2e1a313
SPIDERCLIENT <modingcypress@gmail.com>
2024-11-26 22:10:53 -0600

    BigBubbles.cs

---
+++ Visual/BigBubbles.cs
@@ -0,0 +1,5 @@
+public static void BigBubbles()//UND
+{
+    GameObject.Find("Player Objects/Player VR Controller/GorillaPlayer/TurnParent/Main Camera/UnderwaterVisualEffects/UnderwaterParticleEffects/UnderwaterBubbles").SetActive(true);
+    GameObject.Find("Player Objects/Player VR Controller/GorillaPlayer/TurnParent/Main Camera/UnderwaterVisualEffects/UnderwaterParticleEffects/UnderwaterBubbles").transform.localScale = new Vector3(55f, 55f, 55f);
+}
