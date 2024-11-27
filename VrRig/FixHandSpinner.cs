e25145d1ebfc778007971e82071656624671af40
SPIDERCLIENT <modingcypress@gmail.com>
2024-11-26 22:39:05 -0600

    FixHandSpinner.cs

---
+++ VrRig/FixHandSpinner.cs
@@ -0,0 +1,5 @@
+public static void FixHandSpinner()//UND
+{
+    GorillaTagger.Instance.offlineVRRig.leftHand.rigTarget.transform.rotation *= Quaternion.Euler(GorillaTagger.Instance.offlineVRRig.leftHand.trackingRotationOffset);
+    GorillaTagger.Instance.offlineVRRig.rightHand.rigTarget.transform.rotation *= Quaternion.Euler(GorillaTagger.Instance.offlineVRRig.rightHand.trackingRotationOffset);
+}
