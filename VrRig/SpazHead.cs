4e080d231dc0c1d8aa38bf68729abeca77d434cd
SPIDERCLIENT <modingcypress@gmail.com>
2024-11-26 22:29:30 -0600

    SpazHead.cs

---
+++ VrRig/SpazHead.cs
@@ -0,0 +1,6 @@
+public static void SpazHead()//UND
+{
+    GorillaTagger.Instance.offlineVRRig.head.trackingRotationOffset.y += 15f;
+    GorillaTagger.Instance.offlineVRRig.head.trackingRotationOffset.x += 15f;
+    GorillaTagger.Instance.offlineVRRig.head.trackingRotationOffset.z += 15f;
+}
