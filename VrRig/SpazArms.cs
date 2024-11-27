93254957db79d5ab4247f41d154a6a1b7efecd2a
SPIDERCLIENT <modingcypress@gmail.com>
2024-11-26 22:39:25 -0600

    SpazArms.cs

---
+++ VrRig/SpazArms.cs
@@ -0,0 +1,5 @@
+public static void SpazArms()//UND
+{
+    GorillaTagger.Instance.leftHandTransform.position = new Vector3(Random.Range(0f, 999999f), Random.Range(0f, 999999f), Random.Range(0f, 999999f));
+    GorillaTagger.Instance.rightHandTransform.position = new Vector3(Random.Range(0f, 999999f), Random.Range(0f, 999999f), Random.Range(0f, 999999f));
+}
