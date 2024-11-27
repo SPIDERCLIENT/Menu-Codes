a2ebe3bf8103d92fd229e12d78d10ecd9def7a12
SPIDERCLIENT <modingcypress@gmail.com>
2024-11-26 21:14:19 -0600

    RemovePlatforms.cs

---
+++ Visual/RemovePlatforms.cs
@@ -0,0 +1,78 @@
+public static void RemovePlatforms()//UND
+{
+    if (ControllerInputPoller.instance.rightGrab)
+    {
+        for (int j = 1; j > 0; j++)
+        {
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+        }
+        for (int k = 1; k > 0; k++)
+        {
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+        }
+        for (int l = 1; l > 0; l++)
+        {
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+        }
+        for (int m = 1; m > 0; m++)
+        {
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+        }
+        for (int n = 1; n > 0; n++)
+        {
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+        }
+        for (int num = 1; num > 0; num++)
+        {
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+        }
+        for (int num2 = 1; num2 > 0; num2++)
+        {
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+        }
+        for (int num3 = 1; num3 > 0; num3++)
+        {
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+            GameObject.Find("plat").SetActive(false);
+        }
+    }
+}
