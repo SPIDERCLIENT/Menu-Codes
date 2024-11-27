74f175cd2269d7d7bd2497291252ec65a3784a1a
SPIDERCLIENT <modingcypress@gmail.com>
2024-11-26 20:38:53 -0600

    CubeMonkes.cs

---
+++ Visual/CubeMonkes.cs
@@ -0,0 +1,24 @@
+public static void CubeMonkes()// UND
+{
+    if (PhotonNetwork.CurrentRoom != null)
+    {
+        foreach (NetPlayer player in PhotonNetwork.PlayerListOthers)
+        {
+            VRRig vrrig = GorillaGameManager.instance.FindPlayerVRRig(player);
+            if (vrrig != null && !vrrig.isOfflineVRRig && !vrrig.isMyPlayer)
+            {
+                GameObject gameObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                Object.Destroy(gameObject.GetComponent<BoxCollider>());
+                Object.Destroy(gameObject.GetComponent<Rigidbody>());
+                Object.Destroy(gameObject.GetComponent<Collider>());
+                gameObject.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.magenta);
+                gameObject.transform.rotation = Quaternion.identity;
+                gameObject.transform.localScale = new Vector3(2f, 2f, 2f);
+                gameObject.transform.position = vrrig.transform.position;
+                gameObject.GetComponent<MeshRenderer>().material = vrrig.mainSkin.material;
+                Object.Destroy(gameObject, Time.deltaTime);
+
+            }
+        }
+    }
+}
