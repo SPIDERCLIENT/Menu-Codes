2258af06702f2fb02e983c016e15e0e7a01a5a5a
SPIDERCLIENT <modingcypress@gmail.com>
2024-11-26 21:31:48 -0600

    InfShinyRocks.cs

---
+++ Visual/InfShinyRocks.cs
@@ -0,0 +1,6 @@
+public static void InfShinyRocks()//UND
+{
+    CosmeticsController.instance.numShinyRocksToBuy = 9999999;
+    CosmeticsController.instance.numShinyRocksToBuy = 9999999;
+    CosmeticsController.instance.currencyBalance = 9999999;
+}
