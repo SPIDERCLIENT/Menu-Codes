cc3e1d2b3b1425e958264e5326decdc3e2a3b26b
SPIDERCLIENT <modingcypress@gmail.com>
2024-11-26 22:29:53 -0600

    FixHead.cs

---
+++ VrRig/FixHead.cs
@@ -0,0 +1,6 @@
+public static void FixHead()
+{
+    GorillaTagger.Instance.offlineVRRig.head.trackingRotationOffset.x = 0f;
+    GorillaTagger.Instance.offlineVRRig.head.trackingRotationOffset.y = 0f;
+    GorillaTagger.Instance.offlineVRRig.head.trackingRotationOffset.z = 0f;
+}
