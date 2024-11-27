296821b468bb0461c39c98b1774bca63609ce9ce
SPIDERCLIENT <modingcypress@gmail.com>
2024-11-26 22:11:08 -0600

    DisableBigBubbles.cs

---
+++ Visual/DisableBigBubbles.cs
@@ -0,0 +1,4 @@
+public static void DisableBigBubbles()//UND
+{
+    GameObject.Find("Player Objects/Player VR Controller/GorillaPlayer/TurnParent/Main Camera/UnderwaterVisualEffects/UnderwaterParticleEffects/UnderwaterBubbles").SetActive(false);
+}
