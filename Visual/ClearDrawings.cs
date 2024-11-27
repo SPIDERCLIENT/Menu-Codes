27fb5811709bb07d6e278e8f0adf619b51ef86de
SPIDERCLIENT <modingcypress@gmail.com>
2024-11-26 21:09:54 -0600

    ClearDrawings.cs

---
+++ Visual/ClearDrawings.cs
@@ -0,0 +1,96 @@
+public static void ClearDrawing()//UND
+{
+    if (ControllerInputPoller.instance.rightGrab)
+    {
+        for (int num24 = 1; num24 > 0; num24++)
+        {
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+        }
+        for (int num25 = 1; num25 > 0; num25++)
+        {
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+        }
+        for (int num26 = 1; num26 > 0; num26++)
+        {
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+        }
+        for (int num27 = 1; num27 > 0; num27++)
+        {
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+        }
+        for (int num28 = 1; num28 > 0; num28++)
+        {
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+        }
+        for (int num29 = 1; num29 > 0; num29++)
+        {
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+        }
+        for (int num30 = 1; num30 > 0; num30++)
+        {
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+        }
+        for (int num31 = 1; num31 > 0; num31++)
+        {
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+        }
+        for (int num32 = 1; num32 > 0; num32++)
+        {
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+        }
+        for (int num33 = 1; num33 > 0; num33++)
+        {
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+            GameObject.Find("Sphere").SetActive(false);
+        }
+    }
+}
