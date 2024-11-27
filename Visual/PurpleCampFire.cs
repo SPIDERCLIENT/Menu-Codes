5fb767311ec09fd5e10078e5a808fd5608bfe5a9
SPIDERCLIENT <modingcypress@gmail.com>
2024-11-26 22:08:47 -0600

    PurpleCampFire.cs

---
+++ Visual/PurpleCampFire.cs
@@ -0,0 +1,7 @@
+public static void PurpleCampFire()//UND
+{
+    GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Props/campfire (1)/GameObject (1)").SetActive(true);
+    GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Props/campfire (1)/GameObject (1)").SetActive(true);
+    GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Props/campfire (1)/speaker").SetActive(true);
+    GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Props/campfire (1)/H_Campfire").SetActive(false);
+}
