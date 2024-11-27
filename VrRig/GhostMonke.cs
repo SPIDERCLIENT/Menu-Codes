3f400c2f099c4dd3a7dbf7e1c2c7e2c1b67c6616
SPIDERCLIENT <modingcypress@gmail.com>
2024-11-26 22:23:41 -0600

    GhostMonke.cs

---
+++ VrRig/GhostMonke.cs
@@ -0,0 +1,25 @@
+public static void GhostMonke()//UND
+{
+    if (ControllerInputPoller.instance.rightControllerIndexFloat > 0f)
+    {
+        GorillaTagger.Instance.offlineVRRig.enabled = false;
+        GameObject gameObject = GameObject.CreatePrimitive(0);
+        Object.Destroy(gameObject.GetComponent<Rigidbody>());
+        Object.Destroy(gameObject.GetComponent<SphereCollider>());
+        gameObject.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        gameObject.transform.position = GorillaTagger.Instance.rightHandTransform.position;
+        gameObject.GetComponent<Renderer>().material.color = SettingsforMenu.MenuColor;
+        GameObject gameObject2 = GameObject.CreatePrimitive(0);
+        Object.Destroy(gameObject2.GetComponent<Rigidbody>());
+        Object.Destroy(gameObject2.GetComponent<SphereCollider>());
+        gameObject2.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        gameObject2.transform.position = GorillaTagger.Instance.leftHandTransform.position;
+        gameObject2.GetComponent<Renderer>().material.color = SettingsforMenu.MenuColor;
+        Object.Destroy(gameObject, Time.deltaTime);
+        Object.Destroy(gameObject2, Time.deltaTime);
+    }
+    else
+    {
+        GorillaTagger.Instance.offlineVRRig.enabled = true;
+    }
+}
