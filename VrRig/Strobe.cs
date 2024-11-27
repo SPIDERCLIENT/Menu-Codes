496377eb051516b85ace3301abae016453cd3b6c
SPIDERCLIENT <modingcypress@gmail.com>
2024-11-26 22:57:53 -0600

    Strobe.cs

--- VrRig/Strobe.cs
+++ VrRig/Strobe.cs
@@ -31,3 +31,41 @@ public static void Strobe()//UND
     }, .09f);
     FlushRpcs();
 }
+
+public static Color colorr;
+
+public static int SelectedRGBColor;
+
+public static float Delay;
+
+public static void StartDelay(Action action, float time)
+{
+    if (Time.time >= Delay + time)
+    {
+        Delay = Time.time;
+        action();
+    }
+}
+
+public static void FlushRpcs()
+{
+    GorillaNot.instance.rpcCallLimit = int.MaxValue;
+    PhotonNetwork.RemoveRPCs(PhotonNetwork.LocalPlayer);
+    PhotonNetwork.OpCleanActorRpcBuffer(PhotonNetwork.LocalPlayer.ActorNumber);
+    PhotonNetwork.OpCleanRpcBuffer(GorillaTagger.Instance.myVRRig.GetView);
+    PhotonNetwork.RemoveBufferedRPCs(GorillaTagger.Instance.myVRRig.ViewID, null, null);
+}
+
+public static void SetPlayerColor(Color color) // Working
+{
+    PlayerPrefs.SetFloat("redValue", color.r);
+    PlayerPrefs.SetFloat("greenValue", color.g);
+    PlayerPrefs.SetFloat("blueValue", color.b);
+    GorillaTagger.Instance.UpdateColor(color.r, color.g, color.b);
+    GorillaTagger.Instance.myVRRig.SendRPC("RPC_InitializeNoobMaterial", RpcTarget.All, new object[]
+    {
+        color.r,
+        color.g,
+        color.b
+    });
+}
